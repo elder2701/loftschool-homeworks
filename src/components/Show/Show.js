a96cdbcc5e76f49ee5e8cc41a725c84cbadf047f
Alexy Rumyancev <elder111@yandex.ru>
2019-04-15 10:42:57 +0300

    Show component was created

---
+++ src/components/Show/Show.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Show extends Component {
+  render() {
+    return (
+      <div className="show">
+        <img className="show-image" src="" alt="" />
+        <h2 className="show-label t-show-name"></h2>
+        
+      </div>
+    );
+  }
+}
+
+export default Show;
